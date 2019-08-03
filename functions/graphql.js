9d70c62656f13e990cc4969e86c328c93de63d38
peer <peer@infi.nl>
2019-08-03 13:25:07 +0200

    can send queries with parameters

--- functions/graphql.js
+++ functions/graphql.js
@@ -3,35 +3,45 @@ const { ApolloServer, gql } = require('apollo-server-lambda')
 // schema
 const typeDefs = gql`
   type Dossier {
+    id: Int!
     title: String
     description: String
   }
   type Query {
     hello: String
     dossiers: [Dossier]
+    dossier(id: Int!): Dossier
   }
 `
 
+const dossiers = [
+  {
+    id: 1,
+    title: 'vietnam war',
+    description: 'oh the horror',
+  },
+  {
+    id: 2,
+    title: 'party',
+    description: 'le party',
+  },
+]
+
+// resolvers receive parameters: (obj, args, context, info)
 const resolvers = {
   Query: {
     hello: () => 'Hello, world!',
-    dossiers: () => [
-      {
-        title: 'vietnam war',
-        description: 'oh the horror',
-      },
-      {
-        title: 'party',
-        description: 'le party',
-      },
-    ],
+    dossiers: () => dossiers,
+    dossier: (obj, args) => {
+      return dossiers.find(d => d.id === args.id)
+    },
   },
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  playground: true, // todo: use env to set false on production
+  playground: true, // todo: use env to set false on production, however...
   introspection: true, // problem: process.env.NODE_ENV is always production
 })
 
